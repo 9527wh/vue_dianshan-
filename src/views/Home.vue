41bc54a568e77a5220d2f758dfebe26bc2812a69
wh <2394085813@qq.com>
2021-07-09 01:17:06 +0800

    登录功能的完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="Home">
+    <h1>1123123</h1>
+    <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('./login')
+      this.$message.error('退出成功')
+    },
+  },
 }
 </script>
+<style lang="less" scoped>
+</style>
